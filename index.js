b995913fea9b0de7a036b9445f54180ee0015682
xudi04 <129602928+xudi04@users.noreply.github.com>
2024-01-28 18:04:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.post("/add", (req,res) => {
     res.redirect("/blocks");
 });
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT,3000, () =>{
     console.log("http//localhost:3000");
 });
