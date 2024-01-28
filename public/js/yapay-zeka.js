224a997a08fcb5fda9a62546b96d86a1f6376802
xudi04 <129602928+xudi04@users.noreply.github.com>
2024-01-28 17:57:22 +0300

    S

--- public/js/yapay-zeka.js
+++
@@ -1,6 +0,0 @@
-const img = document.getElementById('img');  
-handTrack.load().then(model => { 
-    model.detect(img).then(predictions => {
-      console.log('Predictions: ', predictions) // bbox predictions
-    });
-});
